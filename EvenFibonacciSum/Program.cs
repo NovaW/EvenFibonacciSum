d09f8ad616a9fef909b7a9fb085d022ee319904b
Nova Wiffen <nova.wiffen@gmail.com>
2020-01-14 11:17:42 +0200

    Added FibonacciSequence class

--- EvenFibonacciSum/Program.cs
+++ EvenFibonacciSum/Program.cs
@@ -6,7 +6,32 @@ namespace EvenFibonacciSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class FibonacciSequence
+    {
+        private int Xn = 1;
+        private int XnMinusOne = 0;
+
+        public int Current() => Xn;
+
+        public int Next()
+        {
+            var holding = XnMinusOne;
+            XnMinusOne = Xn;
+            Xn += holding;
+            return Xn;
+        }
+
+        public int NextEven()
+        {
+            var num = Next();
+            while (num % 2 != 0)
+            {
+                num = Next();
+            }
+            return num;
         }
     }
 }
